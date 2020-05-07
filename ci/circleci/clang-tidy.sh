727e791b42039a5f294ee7fac31c29432f2c1ee0
mathiascode <mail@mathias.is>
2020-05-07 16:11:20 +0300

    Clean up root folder a bit

--- ci/circleci/clang-tidy.sh
+++ ci/circleci/clang-tidy.sh
@@ -1,4 +1,8 @@
-Checks: '-*,readability-identifier-naming'
+#!/bin/sh
+
+set -e
+
+CONFIG="Checks: '-*,readability-identifier-naming'
 CheckOptions:
 - key:             readability-identifier-naming.PrivateMemberPrefix
   value:           'm_'
@@ -14,4 +18,19 @@ CheckOptions:
 #- key:             readability-identifier-naming.EnumCase
 #  value:           camelBack
 WarningsAsErrors: '*'
-HeaderFilterRegex: '/cuberite/src/\.?[^\.]'
+HeaderFilterRegex: '/cuberite/src/\.?[^\.]'"
+
+FIXES_FILE="tidy-fixes.yaml"
+REGEX="/cuberite/src/\.?[^\.]"
+ARGS="-config $CONFIG -header-filter $REGEX -quiet -export-fixes $FIXES_FILE "$@" $REGEX"
+
+mkdir -p tidy-build
+cd tidy-build
+cmake --target Cuberite -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
+if run-clang-tidy.py $ARGS; then
+	echo "clang-tidy: No violations found"
+else
+	echo "clang-tidy: Found violations"
+	exit 1
+fi
